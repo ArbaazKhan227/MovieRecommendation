80b943c467c5d7051fc37b3cdc1461b1074c124f
ArbaazKhan227 <arbaz.khan@deptagency.com>
2025-01-30 15:34:07 +0530

    Update WebConfig.java

--- RecommendationSystem/src/main/java/com/example/RecommendationSystem/config/WebConfig.java
+++ RecommendationSystem/src/main/java/com/example/RecommendationSystem/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")  // Your frontend URL
+                .allowedOrigins("https://movierecommendation-production-3888.up.railway.app/")  // Your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
